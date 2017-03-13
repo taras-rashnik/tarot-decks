6e7073708122e948ff1db99b926cf94d3aa5865b
Taras Rashnik <Taras_Rashnik@epam.com>
2017-03-13 12:17:19 +0100

    add sessions component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SessionsComponent } from './components/sessions/sessions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SessionsComponent
   ],
   imports: [
     BrowserModule,
