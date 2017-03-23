ae2b34fb9f987bae534d0a5fdc14f80cc30c8742
Taras Rashnik <Taras_Rashnik@epam.com>
2017-03-23 10:21:08 +0100

    add simple style linking

--- src/app/components/decks-pane/decks-pane.component.ts
+++ src/app/components/decks-pane/decks-pane.component.ts
@@ -9,9 +9,35 @@ import { Deck } from "../../model/deck";
 export class DecksPaneComponent implements OnInit {
   decks: Deck[] = [];
 
-  constructor() { 
-    for(let i = 0; i <40; i++){
-      this.decks.push(new Deck(`deck${i}`));
+  constructor() {
+    const width = 83;
+    const height = 141;
+    const deltaWidth = 7.5;
+    const deltaHeight = 6;
+    let left = 0;
+    let top = 0;
+
+    for (let i = 0; i < 11; i++) {
+      top = 0;
+
+      for (let j = 0; j < 5; j++) {
+        let deck: any = new Deck(`deck${i}`);
+        deck.picture = {
+          left: Math.floor(left),
+          top: Math.floor(top),
+          width: width,
+          height: height,
+          url: 'url(../../../assets/decks/deck1/tarot_deck_01.png)'
+        };
+
+        this.decks.push(deck);
+
+        top -= height;
+        top -= deltaHeight;
+      }
+
+      left -= width;
+      left -= deltaWidth;
     }
   }
 
