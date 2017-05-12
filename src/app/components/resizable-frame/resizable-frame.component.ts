8cd4f01632ebc00be32a5be186402bd74dbabfbf
Taras Rashnik <Taras_Rashnik@epam.com>
2017-05-12 11:49:13 +0200

    add location and size props to resizable-frame

--- src/app/components/resizable-frame/resizable-frame.component.ts
+++ src/app/components/resizable-frame/resizable-frame.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-resizable-frame',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResizableFrameComponent implements OnInit {
 
+  @Input() left: number = 10;
+  @Input() top: number = 10;
+  @Input() width: number = 100;
+  @Input() height: number = 150;
+
+  styles: any = {};
+
   constructor() { }
 
   ngOnInit() {
+    this.styles = {
+      'left.px': `${this.left}`,
+      'top.px': `${this.top}`,
+      'width.px': `${this.width}`,
+      'height.px': `${this.height}`,
+    };
   }
 
 }
