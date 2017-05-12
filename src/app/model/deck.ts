ef332511891c279afff13aae382d8881d020669c
Taras Rashnik <Taras_Rashnik@epam.com>
2017-05-12 10:01:42 +0200

    add back side of card

--- src/app/model/deck.ts
+++ src/app/model/deck.ts
@@ -10,12 +10,24 @@ export class Deck {
   constructor(public deckInfo: DeckInfo) {
     for (let j = 0; j < deckInfo.cardsInColumn; j++) {
       for (let i = 0; i < deckInfo.cardsInRow; i++) {
+        if (this.cards.length >= deckInfo.cardsNumber) {
+          break;
+        }
+
         this.cards.push(new Card(this, j, i));
       }
     }
 
-    this.backSide = new Card(this, 0, 0);
-    this.deckPicture = new Card(this, 0, 0);
+    this.backSide = new Card(this, this.calculateRow(deckInfo.backSideIndex, deckInfo.cardsInRow), this.calculateColumn(deckInfo.backSideIndex, deckInfo.cardsInRow));
+    this.deckPicture = new Card(this, this.calculateRow(deckInfo.deckPictureIndex, deckInfo.cardsInRow), this.calculateColumn(deckInfo.deckPictureIndex, deckInfo.cardsInRow));
+  }
+
+  private calculateRow(index: number, cardsInRow: number) : number {
+    return Math.floor(index / cardsInRow);
+  }
+
+  private calculateColumn(index: number, cardsInRow: number) : number {
+    return Math.floor(index % cardsInRow);
   }
 
 }
