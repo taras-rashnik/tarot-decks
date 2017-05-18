9419273034b02d270417b4af7d41b639b55c3360
Taras Rashnik <Taras_Rashnik@epam.com>
2017-05-18 16:23:46 +0200

    add mouse events handling over RxJS

--- src/app/components/resizable-frame/resizable-frame.component.ts
+++ src/app/components/resizable-frame/resizable-frame.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+import { Observable } from "rxjs/Observable";
 
 @Component({
   selector: 'app-resizable-frame',
@@ -6,6 +7,9 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./resizable-frame.component.css']
 })
 export class ResizableFrameComponent implements OnInit {
+
+  @ViewChild('rotate') rotate: ElementRef;
+  @ViewChild('resize') resize: ElementRef;
 
   @Input() left: number = 10;
   @Input() top: number = 10;
@@ -16,13 +20,48 @@ export class ResizableFrameComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  updateStyles() {
     this.styles = {
       'left.px': `${this.left}`,
       'top.px': `${this.top}`,
       'width.px': `${this.width}`,
       'height.px': `${this.height}`,
     };
+  }
+
+  ngOnInit() {
+    this.updateStyles();
+
+    Observable.fromEvent(this.rotate.nativeElement, 'mousedown')
+      .flatMap((mdevt: MouseEvent) => {
+        mdevt.preventDefault();
+        // console.log(mdevt);
+
+        let initialLeft: number = +this.left;
+        let initialTop: number = +this.top;
+        let startX: number = mdevt.clientX;
+        let startY: number = mdevt.clientY;
+
+        return Observable.fromEvent(document, 'mousemove')
+          .map((mmevt: MouseEvent) => {
+            mmevt.preventDefault();
+
+            let newLeft: number = initialLeft + (mmevt.clientX - startX);
+            let newTop: number = initialTop + (mmevt.clientY - startY);
+            return {
+              x: newLeft,
+              y: newTop
+            };
+          })
+          .takeUntil(Observable.fromEvent(document, 'mouseup'));
+      })
+      .subscribe((p) => {
+        // console.log(p);
+        this.left = p.x;
+        this.top = p.y;
+        this.updateStyles();
+      });
+
   }
 
 }
