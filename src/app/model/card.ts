fa540831ec5387d4fa9c58a82bfea554197b5a8b
Taras Rashnik <Taras_Rashnik@epam.com>
2017-05-22 15:50:37 +0200

    fix shrink

--- src/app/model/card.ts
+++ src/app/model/card.ts
@@ -8,12 +8,9 @@ export class Card {
     let horizontalPercentage = column * 100 / (deck.deckInfo.cardsInRow - 1);
     let verticalPercentage = row * 100 / (deck.deckInfo.cardsInColumn - 1);
     this.styles = {
-      'background-repeat': 'no-repeat',
       'background-position': `${horizontalPercentage}% ${verticalPercentage}%`,
       'background-image': `url(${deck.deckInfo.imageUrl})`,
       'background-size': `${deck.deckInfo.cardsInRow * 100}% ${deck.deckInfo.cardsInColumn * 100}%`,
-      'width': '100%',
-      'height': '100%',
     };
   }
 }
